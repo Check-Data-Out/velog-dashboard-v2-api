438786549fd451943fab605dae9ab1ce1bb3e7f1
HA0N1 <intor48@gmail.com>
2024-11-27 01:14:42 +0900

    feature: add cors

--- src/app.ts
+++ src/app.ts
@@ -7,12 +7,14 @@ dotenv.config();
 const app: Application = express();
 
 app.use(express.json());
-app.use(cors({
-  origin: process.env.ALLOWED_ORIGINS?.split(',') || 'http://localhost:3000',
-  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-  allowedHeaders: ['Content-Type', 'Authorization'],
-  credentials: true
-}));
+app.use(
+  cors({
+    origin: process.env.ALLOWED_ORIGINS?.split(',') || 'http://localhost:3000',
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    credentials: true,
+  }),
+);
 app.get('/', (req, res) => {
   res.send('Hello, V.D.!');
 });
