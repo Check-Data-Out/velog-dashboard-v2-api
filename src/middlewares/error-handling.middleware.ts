3a53c9a36fa1ad05fc9545ceb4ee5b69f2ce6102
HA0N1 <intor48@gmail.com>
2024-12-04 23:13:42 +0900

    modify: 에러 핸들링 미들웨어

--- src/middlewares/error-handling.middleware.ts
+++ src/middlewares/error-handling.middleware.ts
@@ -3,6 +3,6 @@ import logger from 'src/configs/logger.config';
 
 export const errorHandlingMiddleware = (err: Express.CustomError, req: Request, res: Response, next: NextFunction) => {
   logger.error({ message: err.message, code: err.code, path: req.path });
-  res.status(err.code || 500).json({ message: err.message, code: err.code });
+  res.status(err.code || 500).json({ message: err.message, code: err.code || 500 });
   next();
 };
