d00b458778c8ff4e704471d055d3ba5b806e4a8d
HA0N1 <intor48@gmail.com>
2024-12-09 13:32:43 +0900

    feature: log저장

--- src/configs/logger.config.ts
+++ src/configs/logger.config.ts
@@ -1,4 +1,16 @@
+import fs from 'fs';
 import winston from 'winston';
+import winstonDaily from 'winston-daily-rotate-file';
+
+const logDir = `${process.cwd()}/logs`;
+const errorLogDir = `${logDir}/error`;
+
+if (!fs.existsSync(logDir)) {
+  fs.mkdirSync(logDir);
+}
+if (!fs.existsSync(errorLogDir)) {
+  fs.mkdirSync(errorLogDir);
+}
 
 const logger = winston.createLogger({
   format: winston.format.combine(
@@ -11,9 +23,24 @@ const logger = winston.createLogger({
   ),
   transports: [
     new winston.transports.Console({
-      level: process.env.NODE_ENV === 'production' ? 'error' : 'info',
+      level: process.env.NODE_ENV === 'production' ? 'info' : 'debug',
+    }),
+    new winstonDaily({
+      level: 'debug',
+      datePattern: 'YYYY-MM-DD',
+      zippedArchive: true,
+      filename: `%DATE%.log`,
+      dirname: logDir,
+      maxFiles: '7d',
+    }),
+    new winstonDaily({
+      level: 'error',
+      datePattern: 'YYYY-MM-DD',
+      zippedArchive: true,
+      filename: `%DATE%error.log`,
+      dirname: errorLogDir,
+      maxFiles: '7d',
     }),
   ],
 });
-
 export default logger;
