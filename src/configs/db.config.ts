3fe6c31795248d82ba4bd107c230833089c2d943
Nuung <qlgks1@naver.com>
2024-12-25 00:36:15 +0900

    modify: get all post 부분 SQL 수정

--- src/configs/db.config.ts
+++ src/configs/db.config.ts
@@ -7,9 +7,9 @@ const { Pool } = pg;
 dotenv.config();
 
 const pool = new Pool({
+  database: process.env.DATABASE_NAME,
   user: process.env.POSTGRES_USER,
   host: process.env.POSTGRES_HOST,
-  database: process.env.POSTGRES_NAME,
   password: process.env.POSTGRES_PASSWORD,
   port: Number(process.env.POSTGRES_PORT),
   ssl: {
--- src/repositories/post.repository.ts
+++ src/repositories/post.repository.ts
@@ -3,23 +3,32 @@ import logger from '../configs/logger.config';
 import { DBError } from '../exception';
 
 export class PostRepository {
-  constructor(private pool: Pool) {}
+  constructor(private pool: Pool) { }
   async findPostsByUserId(id: number, cursor?: string, limit: number = 5) {
     try {
       const query = `
-      SELECT
-        p.id,
-        p.title,
-        p.updated_at,
-        pds.daily_view_count,
-        pds.daily_like_count
-      FROM posts_post p
-      LEFT JOIN posts_postdailystatistics pds on p.id = pds.post_id
-      WHERE p.user_id = $1
-        ${cursor ? 'AND p.id < $2' : ''}
-      ORDER BY p.updated_at DESC
-      LIMIT ${cursor ? '$3' : '$2'}
-    `;
+        SELECT
+          p.id,
+          p.title,
+          p.updated_at AS post_updated_at,
+          pds.daily_view_count,
+          pds.daily_like_count,
+          pds.date
+        FROM posts_post p
+        LEFT JOIN (
+          SELECT
+            post_id,
+            daily_view_count,
+            daily_like_count,
+            date
+          FROM posts_postdailystatistics
+          WHERE date::date = (CURRENT_DATE AT TIME ZONE 'UTC' AT TIME ZONE 'Asia/Seoul')::date
+        ) pds ON p.id = pds.post_id
+        WHERE p.user_id = $1
+          ${cursor ? 'AND p.id < $2' : ''}
+        ORDER BY p.updated_at DESC
+        LIMIT ${cursor ? '$3' : '$2'}
+      `;
 
       const params = cursor ? [id, cursor, limit] : [id, limit];
       const posts = await this.pool.query(query, params);
@@ -36,6 +45,7 @@ export class PostRepository {
       throw new DBError('전체 post 조회 중 문제가 발생했습니다.');
     }
   }
+
   async getTotalCounts(id: number) {
     try {
       const query = 'SELECT COUNT(*) FROM "posts_post" WHERE user_id = $1';
