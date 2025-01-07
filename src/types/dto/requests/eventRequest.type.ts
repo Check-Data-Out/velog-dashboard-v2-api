0256c245640af78cba681ac08720ae564b75ff86
Nuung <qlgks1@naver.com>
2025-01-07 22:56:45 +0900

    hotfix: api, integration 하기 위해 대응개발 v2

--- src/types/dto/requests/eventRequest.type.ts
+++ src/types/dto/requests/eventRequest.type.ts
@@ -1,10 +1,13 @@
 import { IsEnum, IsNotEmpty } from 'class-validator';
 
 export enum UserEventType {
-  LOGIN = '01',
-  POST_CLICK = '02',
-  POST_GRAPH_CLICK = '03',
-  EXIT = '04',
+  LOGIN = '11', // 로그인 성공
+  NAVIGATE = '12', // 페이지 이동 (헤더 클릭 등)
+  LOGOUT = '13', // 로그아웃
+  SECTION_INTERACT_MAIN = '21', // 메인 페이지 - 통계 블록 열림/닫힘
+  SORT_INTERACT_MAIN = '22', // 메인 페이지 - 정렬(오름차순, 방식) 선택
+  REFRESH_INTERACT_MAIN = '23', // 메인 페이지 - 새로고침 버튼
+  SORT_INTERACT_BOARD = '31', // 리더보드 페이지 - 정렬 방식 선택
   NOTHING = '99',
 }
 
