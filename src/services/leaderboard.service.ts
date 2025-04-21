4d79f7deb3dd32534624d621eec7c76873350c35
ooheunda <ooheunda@gmail.com>
2025-04-22 00:01:58 +0900

    modify: Leaderboard Repo 쿼리시 오는 RawResult 타입 정의
    any로 두었을 때 lint 검증 및  실행은 잘 되었으나, 테스트 코드 작성시 에러 나서 내부에 정의

--- src/services/leaderboard.service.ts
+++ src/services/leaderboard.service.ts
@@ -1,6 +1,10 @@
 import logger from '@/configs/logger.config';
 import { LeaderboardRepository } from '@/repositories/leaderboard.repository';
-import { LeaderboardResponseData } from '@/types/dto/responses/leaderboardResponse.type';
+import {
+  LeaderboardPostType,
+  LeaderboardResponseData,
+  LeaderboardUserType,
+} from '@/types/dto/responses/leaderboardResponse.type';
 
 export class LeaderboardService {
   constructor(private leaderboardRepo: LeaderboardRepository) {}
@@ -23,32 +27,58 @@ export class LeaderboardService {
   }
 
   private mapRawResultToLeaderboardResponseData(rawResult: unknown[], type: string): LeaderboardResponseData {
-    const result = { posts: null, users: null };
+    const result: LeaderboardResponseData = { posts: null, users: null };
 
     if (type === 'post') {
-      result.posts = rawResult.map((post) => ({
-        id: post.id,
-        title: post.title,
-        slug: post.slug,
-        totalViews: post.total_views,
-        totalLikes: post.total_likes,
-        viewDiff: post.view_diff,
-        likeDiff: post.like_diff,
-        releasedAt: post.released_at,
-      }));
+      result.posts = (rawResult as RawPostResult[]).map(
+        (post): LeaderboardPostType => ({
+          id: post.id,
+          title: post.title,
+          slug: post.slug,
+          totalViews: post.total_views,
+          totalLikes: post.total_likes,
+          viewDiff: post.view_diff,
+          likeDiff: post.like_diff,
+          releasedAt: post.released_at,
+        }),
+      );
     } else {
-      result.users = rawResult.map((user) => ({
-        id: user.id,
-        email: user.email,
-        totalViews: user.total_views,
-        totalLikes: user.total_likes,
-        totalPosts: user.total_posts,
-        viewDiff: user.view_diff,
-        likeDiff: user.like_diff,
-        postDiff: user.post_diff,
-      }));
+      result.users = (rawResult as RawUserResult[]).map(
+        (user): LeaderboardUserType => ({
+          id: user.id,
+          email: user.email,
+          totalViews: user.total_views,
+          totalLikes: user.total_likes,
+          totalPosts: user.total_posts,
+          viewDiff: user.view_diff,
+          likeDiff: user.like_diff,
+          postDiff: user.post_diff,
+        }),
+      );
     }
 
     return result;
   }
+}
+
+interface RawPostResult {
+  id: number;
+  title: string;
+  slug: string;
+  total_views: number;
+  total_likes: number;
+  view_diff: number;
+  like_diff: number;
+  released_at: string;
+}
+
+interface RawUserResult {
+  id: number;
+  email: string;
+  total_views: number;
+  total_likes: number;
+  total_posts: number;
+  view_diff: number;
+  like_diff: number;
+  post_diff: number;
 }
