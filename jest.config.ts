2d3596f960bde402af74b6e0221b80678c276f8b
Nuung <qlgks1@naver.com>
2026-08-29 17:21:31 +0900

    refactor: 통합 테스트를 기본 test 스위트에서 분리

--- jest.config.ts
+++ jest.config.ts
@@ -13,6 +13,9 @@ const config: Config = {
   },
   verbose: true,
   modulePathIgnorePatterns: ['<rootDir>/dist/', '<rootDir>/node_modules/'],
+  // 통합 테스트는 실제 DBMS 연결이 필요하므로 기본 실행에서 제외한다.
+  // 실행하려면 `pnpm test:integration` 을 사용한다.
+  testPathIgnorePatterns: ['/__test__/integration/'],
 };
 
 export default config;
